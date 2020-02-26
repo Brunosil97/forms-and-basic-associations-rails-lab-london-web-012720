15a655920398d55277c867ad25181dddbcff4ad4
Bruno Silva <brunosilvinosilva@gmail.com>
2020-02-26 15:27:51 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist 
+  belongs_to :genre 
+  has_many :notes
+
+  accepts_nested_attributes_for :notes, :reject_if => proc {|attr| attr[:content].blank?}
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+
+  def artist_name 
+    self.artist ? self.artist.name : nil 
+  end 
 end
